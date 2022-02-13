6df0b564b297c284d638c381afb3332bbbf7add0
Onur <onurpulat.op@gmail.com>
2022-02-13 17:55:38 +0300

    Update PlatformerControllerRB.cs

--- Movement/2D/PlatformerControllerRB.cs
+++ Movement/2D/PlatformerControllerRB.cs
@@ -2,17 +2,30 @@ using UnityEngine;
 
 public class PlatformerControllerRB : Monobehaivour
 {
-  private Rigidbody2D rb;
-  
-  private float movementSpeed = 5;
-  
-  private void Start()
-  {
-    rb = GetComponent<Rigidbody2D>();
-  }
-  
-  private void FixedUpdate()
-  {
-    rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
-  }
+	[SerializeField]
+	private float movementSpeed;
+
+	private Rigidbody2D rb;
+	
+	private float movementInputDirection;
+	
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	private void Update()
+	{
+		CheckInputs();
+	}
+	
+	private void CheckInputs()
+	{
+		movementInputDirection = Input.GetAxisRaw("Horizontal");
+	}
+	
+	private void ApplyMovement()
+	{
+		rb.velocity = new Vector3(movementInputDirection * movementSpeed, rb.velocity.y);
+	}
 }
